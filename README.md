e19c464635d490ed2610d2e73c0b139308a91977
ThuRein-110 <thureinoo@Thus-MacBook-Air.local>
2025-10-19 15:03:37 +0700

    Initial commit for Database Final Project

---
+++ README.md
@@ -0,0 +1,8 @@
+I will upload detail in future in this file for now i just finished the function about Reg and Login form basic function with security future.[6731501110] 9/176/2025
+
+LeaMonDog - Diagram
+ShinKhant - Html/ css design for home page  ( responsive)
+
+Pages_2vr is final version for now and Don't do anythings to the Pages
+
+Updated
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Port Authority Access</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Port Authority</h1>
+        <div class="button-group">
+            <button onclick="location.href='Port-Authority/admin_panel/login_admin.php'" class="btn admin">Hello Admin</button>
+            <button onclick="location.href='Port-Authority/Login&Reg/Pages_2vr/index.php'" class="btn user">Hello User</button>
+        </div>
+    </div>
+</body>
+</html>
